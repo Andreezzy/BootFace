ba602d186e4867292df3eabd4d893d22f8d54974
andreezzy <frandres333@gmail.com>
2016-11-30 18:07:04 -0500

    change version python for deploy

--- handler.py
+++ handler.py
@@ -18,11 +18,11 @@ def text_message(recipient_id, message_text):
 	}
 	return message_data
 
-def call_send_API(data, token):
+def call_send_API( data, token ):
 	res = requests.post('https://graph.facebook.com/v2.6/me/messages',
-		params = {'access_token': token},
-		data = json.dumps(data),
-		headers = { 'Content-type' : 'application/json' }
-		)
+					params = {'access_token': token },
+					data = json.dumps(data),
+					headers = { 'Content-type': 'application/json' }
+					)
 	if res.status_code == 200:
-		print("Mensaje enviado correctamente")
+		print("El mensaje fue enviado exitosamente!")
--- manage.py
+++ manage.py
@@ -8,30 +8,32 @@ from config import DevelopmentConfig
 from handler import recived_message
 
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
+app.config.from_object( DevelopmentConfig )
 
-@app.route('/webhook', methods = ['GET'])
+@app.route('/webhook', methods = ['GET', 'POST'])
 def webhook():
 	if request.method == 'GET':
 		verify_token = request.args.get('hub.verify_token', '')
 		if verify_token == app.config['SECRET_KEY']:
 			return request.args.get('hub.challenge', '')
-		return 'Error'
+		return 'Error al validar el secreto'
+
 	elif request.method == 'POST':
-		payload = request.get_data()
+		payload  = request.get_data()
+		print(payload)
 		data = json.loads(payload)
 
 		for page_entry in data['entry']:
 			for message_event in page_entry['messaging']:
 				if 'message' in message_event:
-					recived_message(message_event, app.config['SECRET_KEY'])
-					#evento = message_event['message']
-					#print(evento['text'])
+					recived_message( message_event , app.config['PAGE_ACCESS_TOKEN'])
+					
 		return "ok"
+
 
 @app.route('/', methods = ['GET'])
 def index():
-	return "text"
+	return 'Hola al curso de Bot!'
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
