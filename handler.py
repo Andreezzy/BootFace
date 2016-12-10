177a7f44af52779b7846fd7a65749e4357ece7c7
andreezzy <frandres333@gmail.com>
2016-12-09 21:21:03 -0500

    Agregando burbujas 6

--- handler.py
+++ handler.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 encoding=utf8
-import sys reload(sys) sys.setdefaultencoding('utf8')
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 import requests
 import json
